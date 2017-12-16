c2dfd09ae7177694c0ee14dd3d29cc7aa4b817c3
xyyx <xyyx@mail.ru>
2017-12-16 22:07:37 +0800

    Import Lineage doze from op3 tree

--- msm8953.mk
+++ msm8953.mk
@@ -362,4 +362,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 PRODUCT_PACKAGES += \
-    XiaomiParts
+        XiaomiParts \
+        XiaomiDoze
